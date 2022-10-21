38f48cea4bb9940d9cc6c8a36d6f99587f36da7a
Moses Benjamin <benmos16@gmail.com>
2022-10-21 17:29:44 +0100

    home page update

--- src/script.js
+++ src/script.js
@@ -1,45 +1,76 @@
 var header;
 header = document.querySelector('header');
+var navitem;
+navitem = document.querySelectorAll("nav > ul > li a");
 var count = 0;
 var image_url = [
-    'back.jpg',
+    // 'back.jpg',
     'back2.jpg',
-    'back3.jpg',
+    //'back3.jpg',
     'back4.jpg',
-    'back5.jpg',
+    // 'back5.jpg',
     'back6.jpg',
-    'back7.jpg',
+    // 'back7.jpg',
 ];
+function checkbgcolor(count) {
+    console.log("bg", count);
+    if (count === 0) {
+        navitem.forEach(function (a) { return a.style.color = "rgb(116,79,99)"; });
+    }
+    else {
+        navitem.forEach(function (a) { return a.style.color = "black"; });
+    }
+}
+//function to display menu bar on mobile
+var menuitems = document.querySelector("header > .mobile_header");
+var menu = document.querySelector("header > .menu_bar");
+menu === null || menu === void 0 ? void 0 : menu.addEventListener("click", displayMenu);
+function displayMenu() {
+    menuitems === null || menuitems === void 0 ? void 0 : menuitems.classList.add("display_menu");
+}
+//function to remove menu bar on mobile
+var cancel_menu = document.querySelector("header .cancel_menu");
+cancel_menu === null || cancel_menu === void 0 ? void 0 : cancel_menu.addEventListener("click", removeMenu);
+function removeMenu() {
+    menuitems === null || menuitems === void 0 ? void 0 : menuitems.classList.remove("display_menu");
+}
 var prevDom = document.querySelector('.move > .prev');
 var next = function () {
     if (window.innerWidth >= 350) {
         if (count < image_url.length - 1) {
-            header.style.backgroundImage = "url(img/".concat(image_url[++count], ")");
+            ++count;
+            header.style.backgroundImage = "url(img/".concat(image_url[count], ")");
+            checkbgcolor(count);
         }
         else {
             count = 0;
             header.style.backgroundImage = "url(img/".concat(image_url[count], ")");
+            checkbgcolor(count);
         }
-        console.log(count);
-        console.log(window.innerWidth);
+    }
+    else {
+        header.style.backgroundImage = "";
     }
 };
 var nextDom = document.querySelector('.move > .next');
 var prev = function () {
     if (window.innerWidth >= 350) {
         if (count > 0) {
-            header.style.backgroundImage = "url(img/".concat(image_url[--count], ")");
-            console.log(count);
+            --count;
+            header.style.backgroundImage = "url(img/".concat(image_url[count], ")");
+            checkbgcolor(count);
         }
         else {
             count = image_url.length - 1;
             header.style.backgroundImage = "url(img/".concat(image_url[count], ")");
+            checkbgcolor(count);
         }
-        console.log(count);
-        console.log(window.innerWidth);
+    }
+    else {
+        header.style.backgroundImage = "";
     }
 };
-var inter = setInterval(next, 2000);
+var inter = setInterval(next, 40000);
 window.innerWidth <= 350 ? clearInterval(inter) : null; //stop the interval on smaller screen
 prevDom === null || prevDom === void 0 ? void 0 : prevDom.addEventListener('click', prev);
 nextDom === null || nextDom === void 0 ? void 0 : nextDom.addEventListener('click', next);
--- src/script.ts
+++ src/script.ts
@@ -1,51 +1,82 @@
 let header: any;
 header = document.querySelector('header');
+let navitem: any;
+navitem = document.querySelectorAll("nav > ul > li a")
+
 
 let count: number = 0;
 const image_url: string[] = [
-  'back.jpg',
-  'back2.jpg',
-  'back3.jpg',
+  // 'back.jpg',
+   'back2.jpg',
+  //'back3.jpg',
   'back4.jpg',
-  'back5.jpg',
+  // 'back5.jpg',
   'back6.jpg',
-  'back7.jpg',
+  // 'back7.jpg',
 ];
+
+function checkbgcolor(count : number) : void{
+  console.log("bg", count)
+  if(count === 0){
+   navitem.forEach(a => a.style.color = "rgb(116,79,99)")
+  }else{
+   navitem.forEach(a => a.style.color = "black")
+  }
+}
+
+//function to display menu bar on mobile
+const menuitems = document.querySelector("header > .mobile_header")
+const menu = document.querySelector("header > .menu_bar")
+
+menu?.addEventListener("click", displayMenu)
+function displayMenu() {
+  menuitems?.classList.add("display_menu")
+}
+
+//function to remove menu bar on mobile
+const cancel_menu = document.querySelector("header .cancel_menu")
+cancel_menu?.addEventListener("click", removeMenu)
+function removeMenu(){
+  menuitems?.classList.remove("display_menu")
+}
+
 
 const prevDom = document.querySelector('.move > .prev');
 const next = (): void => {
   if (window.innerWidth >= 350) {
     if (count < image_url.length - 1) {
-      header.style.backgroundImage = `url(img/${image_url[++count]})`;
+      ++count
+      header.style.backgroundImage = `url(img/${image_url[count]})`;
+      checkbgcolor(count);
     } else {
       count = 0;
       header.style.backgroundImage = `url(img/${image_url[count]})`;
+      checkbgcolor(count);
     }
-    console.log(count);
-    console.log(window.innerWidth);
   } else{
     header.style.backgroundImage = ""
   }
+
 };
 
 const nextDom = document.querySelector('.move > .next');
 const prev = (): void => {
     if (window.innerWidth >= 350){
         if (count > 0) {
-            header.style.backgroundImage = `url(img/${image_url[--count]})`;
-            console.log(count);
+            --count;
+            header.style.backgroundImage = `url(img/${image_url[count]})`;
+            checkbgcolor(count);
           } else {
             count = image_url.length - 1;
             header.style.backgroundImage = `url(img/${image_url[count]})`;
+            checkbgcolor(count);
           }
-          console.log(count);
-          console.log(window.innerWidth);
     }
     else{
       header.style.backgroundImage = ""
     }
 };
-let inter = setInterval(next, 5000);
+let inter = setInterval(next, 40000);
 window.innerWidth <= 350 ? clearInterval(inter) : null; //stop the interval on smaller screen
 prevDom?.addEventListener('click', prev);
 nextDom?.addEventListener('click', next);
